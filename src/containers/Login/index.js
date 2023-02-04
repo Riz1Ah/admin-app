446770e3e5cafe3dc6bdf22f03ba096cca897a77
Rizwan <fbriz007@gmail.com>
2023-02-04 13:58:58 +0530

    Create the layouts with all the fields

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Container,Form, Button, Row, Col } from 'react-bootstrap';
+import Layout from '../../components/Layout';
+import Input from '../../components/UI/Input';
 
 const Login = (props) => {
-    return ( 
-        <div>Login</div>
-     );
+    return (
+        <Layout>
+            <Container>
+                <Row style={{margin:'50px'}}>
+                    <Col md={{span:6, offset:3}}>
+                    <Form>
+                    <Input
+                                label='Username'
+                                placeholder='Username'
+                                value=''
+                                type='text'
+                                onChange={()=>{}}
+                            />
+                    <Input
+                                label='Password'
+                                placeholder='Password'
+                                value=''
+                                type='text'
+                                onChange={()=>{}}
+                            />
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+                    </Col>
+                </Row>
+                
+            </Container>
+        </Layout>
+    );
 }
- 
+
 export default Login;
